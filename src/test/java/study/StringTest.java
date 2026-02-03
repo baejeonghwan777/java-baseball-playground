07380275aae2de93bfadd749dc3e5defe8ef484a
baejeonghwan777 <baejeonghwon777@gmail.com>
2026-02-03 14:27:59 +0900

    string 학습 테스트 완료

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,6 +1,9 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,4 +13,35 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void seperate() {
+        String[] number = "1,2".split(",");
+        assertThat(number).contains("1","2");
+    }
+
+    @Test
+    void seperate2() {
+        String[] number = "1".split(",");
+        assertThat(number).containsExactly("1");
+    }
+
+    @Test
+    void remove() {
+        String number = "(1,2)".substring(1,4);
+        assertThat(number).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("문자열 추출 테스트")
+    void extract() {
+        int num = 2;
+        String str = "abc";
+        if(num < 0 || num >= str.length()) {
+            throw new IndexOutOfBoundsException();
+        } else if(num >= 0 && num < str.length()) {
+            char actual = str.charAt(num);
+        }
+    }
+
 }
