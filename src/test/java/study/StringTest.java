6b49df346fd2b6f5ed2ae53013a892ef7ede045f
baejeonghwan777 <baejeonghwon777@gmail.com>
2026-02-03 16:13:34 +0900

    SetCollection 테스트 완료

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,8 +3,6 @@ package study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Scanner;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
